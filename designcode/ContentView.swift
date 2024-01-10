27abbd84f0959b9151c158f40391f6a5605fbb9d
aulia octaviani <aulia.octaviani@digitalsekuriti.id>
2024-01-10 09:40:53 +0700

    Initial Commit

---
+++ designcode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  designcode
+//
+//  Created by Aulia Octaviani on 10/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ designcode/designcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  designcodeApp.swift
+//  designcode
+//
+//  Created by Aulia Octaviani on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct designcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
